e2aa7ed07e9268c56417c10d0c9dcd643cb2e691
Crend King <975235+CrendKing@users.noreply.github.com>
2019-05-10 07:44:09 -0700

    Add title

--- unloader/index.js
+++ unloader/index.js
@@ -1,16 +1,26 @@
 window.addEventListener('load', ev => {
-    const urlParams = new URLSearchParams(window.location.search);
+    const urlParams = new URLSearchParams(window.location.search)
 
-    document.title = `(U) ${urlParams.get('title')}`;
-    document.querySelector('link[rel *= "icon"]').href = urlParams.get('favicon');
+    let title
+    if (urlParams.has('title')) {
+        title = urlParams.get('title')
+        document.title = `(U) ${title}`
+    } else {
+        title = ''
+        document.title = ''
+    }
+
+    document.querySelector('link[rel *= "icon"]').href = urlParams.get('favicon')
     document.body.addEventListener('dblclick', () => {
-        location = urlParams.get('location');
-    });
+        location = urlParams.get('location')
+    })
     document.body.addEventListener('keypress', (event) => {
         if (event.key === ' ' || event.key === 'Enter') {
-            location = urlParams.get('location');
+            location = urlParams.get('location')
         }
-    });
-    document.getElementById('location').value = urlParams.get('location');
+    })
+
+    document.getElementById('title').value = title
+    document.getElementById('location').value = urlParams.get('location')
 });
 
