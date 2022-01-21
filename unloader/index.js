95d09d5295c62707a86af831e836d6ae066f500a
Crend King <975235+CrendKing@users.noreply.github.com>
2022-01-20 17:00:15 -0800

    Support restore time

--- unloader/index.js
+++ unloader/index.js
@@ -1,6 +1,23 @@
 window.addEventListener('load', ev => {
     const urlParams = new URLSearchParams(window.location.search)
 
+    const originalLocation = urlParams.get('location')
+    const restore = () => {
+        document.getElementById('state').textContent = 'Loading ...'
+        window.location = originalLocation
+    }
+
+    let restoreTime = urlParams.get('restoreTime')
+    if (restoreTime !== null) {
+        if (restoreTime <= 0) {
+            restoreTime = Date.now() - restoreTime
+        }
+
+        if (Date.now() >= restoreTime) {
+            restore()
+        }
+    }
+
     let title
     if (urlParams.has('title')) {
         title = urlParams.get('title')
@@ -11,15 +28,13 @@ window.addEventListener('load', ev => {
     }
 
     document.getElementById('title').textContent = title
-    document.getElementById('location').textContent = urlParams.get('location')
+    document.getElementById('location').textContent = originalLocation
 
     document.querySelector('link[rel = "icon"]').href = urlParams.get('favicon')
-    document.body.addEventListener('dblclick', () => {
-        window.location = urlParams.get('location')
-    })
+    document.body.addEventListener('dblclick', restore)
     document.body.addEventListener('keypress', (event) => {
         if (event.key === ' ' || event.key === 'Enter') {
-            window.location = urlParams.get('location')
+            restore()
         }
     })
 });
